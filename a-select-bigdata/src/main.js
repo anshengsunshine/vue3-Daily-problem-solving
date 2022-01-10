6802ad0b195f6ab53ff2a958c9adb07ed497fcb9
jiajing <jiajing@zhiweidata.com>
2022-01-10 09:47:20 +0800

    去掉about页面;按需引入ant-design-vue的select

--- a-select-bigdata/src/main.js
+++ a-select-bigdata/src/main.js
@@ -2,5 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { Select } from "ant-design-vue";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = create(App)
+
+app.use(store).use(router).use(Select).mount("#app");
